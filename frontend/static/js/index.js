d40574e15852ed7aedeffed546f6b0f2d99c50da
William Buechele <buechelewd@gmail.com>
2021-02-14 12:56:53 -0700

    progress

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,20 +1,36 @@
-   const navigateTo = url => {
+import Dashboard from "./views/Dashboard.js";
+
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+  
+  const navigateTo = url => {
        history.pushState(null, null, url);
        router();
    };
    
+   //Creating router
    const router = async () => {
+       //Array of paths for the router to direct to, executes code when directed to each
         const routes = [
-            { path: "/", view: () => console.log("Viewing Dashboard")},
-            { path: "/posts", view: () => console.log("Viewing Posts")},
-            { path: "/settings", view: () => console.log("Viewing Settings")},
+            { path: "/", view: () => Dashboard}
+            //{ path: "/posts", view: () => console.log("Viewing Posts")},
+            //{ path: "/settings", view: () => console.log("Viewing Settings")},
         ];
 
         // test each route for potential match
         const potentialMatches = routes.map(route => {
             return {
                 route: route,
-                isMatch: location.pathname === route.path
+                result: location.pathname.match(pathToRegex(route.path))
             };
         });
 
@@ -23,13 +39,18 @@
         if (!match) {
             match = {
                 route: routes[0],
-                isMatch: true
+                result: [location.pathname]
             };
         }
+
+        const view = new match.route.view(getParams(match));
+
+        document.querySelector("#app").innerHTML = await view.getHtml();
 
         console.log(match.route.view());
     };
 
+        window.addEventListener("popstate", router);
 
     document.addEventListener("DOMContentLoaded", () => {
         document.body.addEventListener("click", e =>{
@@ -38,5 +59,6 @@
                 navigateTo(e.target.href)
             };
         })
+        
         router();
     });
